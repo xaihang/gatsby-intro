c304feeb8b88f9dd0a30af8b08e599477c8afb2c
Xai Hang <108967069+xaihang@users.noreply.github.com>
2023-04-02 12:39:30 -0500

    added profile img

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,7 @@
-import React from "react"
-import { Link } from 'gatsby'
+import React from "react";
+import { Link } from "gatsby";
 import Layout from "../components/Layout";
-import '../styles/home.css'
-
+import "../styles/home.css";
 
 export default function Home() {
   return (
@@ -12,8 +11,15 @@ export default function Home() {
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
           <p>Full Stack Software Developer from Minneapolis, MN</p>
-          <Link className="btn" to="/projects">My Portfolio Projects</Link>
+          <Link className="btn" to="/projects">
+            My Portfolio Projects
+          </Link>
         </div>
+        <img
+          src="/profile.jpeg"
+          alt="profile"
+          style={{ maxWidth: "300px", borderRadius: "50%" }}
+        />
       </section>
     </Layout>
   );
